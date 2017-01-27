cf7df034d1f77e57e7ed548ecc15837e549fafcb
sicklerin <jansierpina@gmail.com>
2017-01-27 18:59:47 +0100

    haslist complete

--- src/definedirective.h
+++ src/definedirective.h
@@ -3,7 +3,6 @@
 */
 #include <stdbool.h>
 
-void printDirectives();
 int resolveDefineDirectives(char **er, char **output, const char *buffer, int bufferLength, int line);
 int addDirective(char **er, const char *line, int length, int defLine);
 bool isDefineDirective(const char *line, int length);
